4670836e0485b67e6ed97e7c15012fedb86fc440
Marek Jóźwiak <hexthecoder@gmail.com>
2026-08-14 02:20:53 +0200

    build(lab): use static adapter

--- apps/lab/vite.config.ts
+++ apps/lab/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -13,12 +13,14 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		})
 	],
+	build: {
+		rolldownOptions: {
+			checks: { pluginTimings: false }
+		}
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
