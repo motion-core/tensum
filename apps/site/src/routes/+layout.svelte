7dc2a940d7025f9eb88dcd2d340f5ead402d215e
Marek Jóźwiak <hexthecoder@gmail.com>
2026-08-26 00:51:23 +0200

    fix(site): disable tooltips on touch devices

--- apps/site/src/routes/+layout.svelte
+++ apps/site/src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { MediaQuery } from 'svelte/reactivity';
 	import * as Tooltip from '$lib/components/ui/tooltip';
 	import './layout.css';
 
 	let { children } = $props();
+	const supportsTooltips = new MediaQuery(
+		'(min-width: 768px) and (hover: hover) and (pointer: fine)'
+	);
 </script>
 
-<Tooltip.Provider delayDuration={350} skipDelayDuration={100}>
+<Tooltip.Provider delayDuration={350} skipDelayDuration={100} disabled={!supportsTooltips.current}>
 	{@render children()}
 </Tooltip.Provider>
